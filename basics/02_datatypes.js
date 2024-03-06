1787a04be6fe68311ea4eee6768f7fb9e3aa1a93
Rock <dhanjee.don@gmail.com>
2024-03-07 04:52:55 +0530

    datatypes in js

---
+++ basics/02_datatypes.js
@@ -0,0 +1,18 @@
+ "use strict"; //treat all the js code as newer version
+
+
+//  alert(43 + 5) we r using it in node js thats why it will show not defined,(not using it in browser)
+
+
+// number ==> (2 to power 53)-- you can take input in number 
+// bigint ==> biger no. than 2 to power 53
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value 
+// undefined
+// symbol ==>unique
+
+// object 
+
+console.log(typeof undefined); // output -- undefined
+console.log(typeof null); // output -- object
