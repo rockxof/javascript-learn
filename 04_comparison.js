b3d3229e6ab3baa4b767128496c46ce7426e0efa
Rock <dhanjee.don@gmail.com>
2024-03-10 05:44:17 +0530

    comparison

---
+++ 04_comparison.js
@@ -0,0 +1,20 @@
+// // console.log(2 > 1);
+// // console.log(2 >= 1);
+// // console.log(2 < 1);
+// // console.log(2 == 1);
+// // console.log(2 != 1);
+
+// // console.log("2" > 1);output = true because its converted "2" into 2
+// // console.log("2" == 2); output = true
+
+// console.log(null > 0);   ===> output : false
+// console.log(null == 0);   ===> output : false
+// console.log(null >= 0);   ===> output : true -->because its converted null into 0 if there is >,>=,<= then its covert into 0
+
+
+// === strict check
+
+console.log("2" === 2); 
+output = false
+
+// conclusion> we should always avoid different datatypes comparision
