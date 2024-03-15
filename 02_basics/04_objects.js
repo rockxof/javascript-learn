aca6203610e3f273d579b707d3e3b39a84137f69
Rock <dhanjee.don@gmail.com>
2024-03-15 06:08:01 +0530

    objects properties

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -6,10 +6,10 @@
 const tinderuser = {}
 // it is not a singleton ----- Literals
 
-// tinderuser.Id = "4569"
-// tinderuser.Name = 'Rock'
-// tinderuser.Age = 22 
-// tinderuser.IsLoggedIn = false
+tinderuser.Id = "4569"
+tinderuser.Name = 'Rock'
+tinderuser.Age = 22 
+tinderuser.IsLoggedIn = false
 
 // console.log(tinderuser);
 // output : { Id: '4569', Name: 'Rock', Age: 22, IsLoggedIn: false }
@@ -27,4 +27,51 @@ const regularUser = {
 
 // this is called an object Nesting
 
-console.log(regularUser.Name.realName.lastName);
+// console.log(regularUser.Name.realName.lastName);
+
+const obj1 =  { 1 : "A" , 2 : "B"}
+const obj2 =  { 3 : "C" , 4 : "D"}
+const obj3 =  { 5 : "E" , 6 : "F"}
+
+// const objParent = Object.assign( {} , obj1 , obj2 , obj3)
+// Object.assign( {Target} , source...) this is used to combine objects
+
+const objParent = { ...obj1, ...obj2, ...obj3}
+// here we r using spread method like last tym we used it in array to combine array
+// Most tym we r gonna use Spread method to combine arrays or objects
+
+
+const user = [
+    {
+        userId : 'rock1',
+        email : 'rock1@email.com',
+        
+    },
+    {
+        userId : 'rock2',
+        email : 'rock2@email.com'
+    },
+    {
+        userId : 'rock3',
+        email : 'rock3@email.com'
+    }
+]
+
+console.log(user[2].email);
+// it will check email/userid ***IMPORTANT try it urself>>>>>>>>>>>>>>>>>>>>>>>>>
+
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+// it shows the keys means in which the value is stored eg: Id, Name, IsLoggedIn
+
+console.log(Object.values(tinderuser));
+// it shows the values present inside the keys eg: '4569', 'Rock', false
+
+console.log(Object.entries(tinderuser));
+// it converts into single arrays eg:  [ 'Id', '4569' ], [ 'Name', 'Rock' ]
+
+
+console.log(tinderuser.hasOwnProperty("IsLoggedIn"));
+// it will check if the property is avialable or not inside the tinder user nd gives output as Boolean
+
