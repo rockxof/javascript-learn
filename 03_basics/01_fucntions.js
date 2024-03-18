95cb95718f8da7294625556b95f1de84baac4a58
Rock <dhanjee.don@gmail.com>
2024-03-18 06:34:12 +0530

    object nd array in function

--- 03_basics/01_fucntions.js
+++ 03_basics/01_fucntions.js
@@ -39,7 +39,49 @@ function newUserMessage(username = "rock2"){
 //  console.log(newUserMessage("rock"));   
 // here it will  overwrite the deafault username nd print whatever aruguments you passed
 
- console.log(newUserMessage());   
+//  console.log(newUserMessage());   
 //  here it will print default name given in parameter
 
- 
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Rest Operator(...)>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+ function myShoppingCartAmount(val1, val2, ...num1){
+    return num1;
+ }
+//  console.log(myShoppingCartAmount(100, 300, 350, 1120));
+
+ //here im using rest operator(...) also known as spread it will converts all the numbers given in arguments
+ // into an array eg: [ 100, 300, 350, 1120 ]
+
+ // IMPORTANT INTERVIEW QUESTIONS
+ // afteer adding (val1, val2) my output is: [ 350, 1120 ] my first 2 aruguments went to val1 nd val 2
+
+//  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Objects nd Array in function>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+const user1 = {
+    name : "rock",
+    age : 22
+}
+
+const user2 = {
+    name : "manjee",
+    age : 18
+}
+
+function userData(anyobject){
+    return `Hi my name is ${anyobject.name} nd my age is ${anyobject.age}`
+
+}
+console.log(userData(user2));
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>array in function<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+const myArray = [
+    30, 40, 50, 60, 70
+]
+
+function data(anyarray){
+    // return myArray[3]
+    return `first num is ${anyarray[2]}`
+}
+console.log(data(myArray));
+
+// using this method we undersood that how to recall anyobject or array into function
