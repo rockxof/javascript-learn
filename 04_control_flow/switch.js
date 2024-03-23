c163ed4106aae33628edafa684d3f68166e3eced
Rock <dhanjee.don@gmail.com>
2024-03-24 05:20:47 +0530

    switch in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,40 @@
+// const month = 5;
+
+// switch (month) {
+//   case 1:
+//     console.log("january");
+//     break;
+//   case 2:
+//     console.log("feb");
+//     break;
+//   case 8:
+//     console.log("aug");
+//     break;
+//   case 12:
+//     console.log("dec");
+//     break;
+//   default:
+//     console.log("Not a valid month");
+//     break;
+// }
+
+const userData = "email"
+
+switch (userData) {
+    case "name":
+        console.log("Rock");
+        break;
+    case "age":
+        console.log(22);
+        break;
+    case "gender":
+        console.log("male");
+        break;
+    case "email":
+        console.log("rock92@haburger.com");
+        break;
+
+    default:
+        console.log("not a valid entry");
+        break;
+}
